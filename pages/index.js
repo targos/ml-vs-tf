e8415d5f354d88acafb8a042381093f54df59412
Michaël Zasso <targos@protonmail.com>
2018-11-08 23:28:19 +0100

    make it configurable

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,64 @@
 import { useState } from 'react';
 import Head from 'next/head';
 
-import execute from '../src/browser';
+import { createMatrices, execute } from '../src/browser';
 
 export default () => {
+  const [iterations, setIterations] = useState(100);
+  const [width, setWidth] = useState(200);
+  const [height, setHeight] = useState(300);
+  const [data, setData] = useState(null);
   const [executing, setExecuting] = useState(false);
   const [results, setResults] = useState(null);
+
+  const handleChangeIterations = (e) => {
+    setIterations(Number(e.target.value));
+  };
+  const handleChangeWidth = (e) => {
+    setWidth(Number(e.target.value));
+  };
+  const handleChangeHeight = (e) => {
+    setHeight(Number(e.target.value));
+  };
+
+  let action;
+  let result;
+  if (!data || data.width !== width || data.height !== height) {
+    result = null;
+    action = (
+      <button
+        type="button"
+        className="border p-2"
+        onClick={() => {
+          setData(createMatrices(width, height));
+        }}
+      >
+        Create matrices
+      </button>
+    );
+  } else {
+    result = executing
+      ? 'Executing...'
+      : results && <Results value={results} />;
+    action = (
+      <button
+        className="border p-2"
+        type="button"
+        disabled={executing}
+        onClick={() => {
+          setExecuting(true);
+          setResults(null);
+          setTimeout(() => {
+            setResults(execute(data, iterations));
+            setExecuting(false);
+          }, 100);
+        }}
+      >
+        Execute
+      </button>
+    );
+  }
+
   return (
     <>
       <Head>
@@ -16,22 +69,45 @@ export default () => {
       </Head>
       <div className="container m-auto p-10">
         <h1 className="mb-6">Demo of ml.js vs TensorFlow.js</h1>
-        <h2 className="mb-4">Multiplying two matrices 200x300 and 300x200</h2>
-        <button
-          className="border p-2"
-          type="button"
-          disabled={executing}
-          onClick={() => {
-            setExecuting(true);
-            setTimeout(() => {
-              execute(setResults);
-              setExecuting(false);
-            }, 10);
-          }}
-        >
-          Execute
-        </button>
-        {results && <Results value={results} />}
+        <h2 className="mb-4">Multiplying two matrices</h2>
+        <div className="mb-2">
+          <label htmlFor="width" className="mr-4">
+            Width
+          </label>
+          <input
+            className="border rounded p-2"
+            type="number"
+            value={width}
+            id="width"
+            onChange={handleChangeWidth}
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="height" className="mr-4">
+            Height
+          </label>
+          <input
+            className="border rounded p-2"
+            type="number"
+            value={height}
+            id="height"
+            onChange={handleChangeHeight}
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="iterations" className="mr-4">
+            Iterations
+          </label>
+          <input
+            className="border rounded p-2"
+            type="number"
+            value={iterations}
+            id="iterations"
+            onChange={handleChangeIterations}
+          />
+        </div>
+        {action}
+        {result}
       </div>
     </>
   );
--- src/browser.js
+++ src/browser.js
@@ -3,45 +3,51 @@
 import { Matrix } from 'ml-matrix';
 import * as tf from '@tensorflow/tfjs-core';
 
-const m1 = Matrix.random(200, 300);
-const m2 = Matrix.random(300, 200);
+export function createMatrices(width, height) {
+  const m1 = Matrix.random(width, height);
+  const m2 = Matrix.random(height, width);
 
-const ma1 = m1.to2DArray();
-const ma2 = m2.to2DArray();
+  const ma1 = m1.to2DArray();
+  const ma2 = m2.to2DArray();
 
-const t1 = tf.tensor(ma1);
-const t2 = tf.tensor(ma2);
+  const t1 = tf.tensor(ma1);
+  const t2 = tf.tensor(ma2);
 
-function getTResult() {
-  const tResult = t1.matMul(t2);
-  return tResult;
+  return {
+    width,
+    height,
+    m1,
+    m2,
+    t1,
+    t2
+  };
 }
 
-function getMResult() {
-  const mResult = m1.mmul(m2);
-  return mResult;
-}
+export function execute(data, iterations) {
+  const { m1, m2, t1, t2 } = data;
+  function getTResult() {
+    const tResult = t1.matMul(t2);
+    return tResult;
+  }
 
-// warmup
-for (var i = 0; i < 10; i++) {
-  getTResult();
-  getMResult();
-}
+  function getMResult() {
+    const mResult = m1.mmul(m2);
+    return mResult;
+  }
 
-export default function execute(setResults) {
   const result = {};
   // timing t
   const startT = performance.now();
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < iterations; i++) {
     getTResult();
   }
   result.t = performance.now() - startT;
 
   // timing m
   const startM = performance.now();
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < iterations; i++) {
     getMResult();
   }
   result.m = performance.now() - startM;
-  setResults(result);
+  return result;
 }
